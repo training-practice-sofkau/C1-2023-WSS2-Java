c984a3dc0a6026ef9df84bb2817d6eabb302dddc
Diego Becerra <diegofer_1110@hotmail.com>
2023-01-31 13:03:06 -0500

    Artist entity done

---
+++ src/main/java/co/com/chartsofka/music/entity/Artist.java
@@ -0,0 +1,13 @@
+package co.com.chartsofka.music.entity;
+
+import java.time.LocalDate;
+
+public class Artist {
+
+    private String artistID;
+    private String country;
+    private LocalDate debutDate;
+    private String enterprise;
+    private String type;
+
+}
