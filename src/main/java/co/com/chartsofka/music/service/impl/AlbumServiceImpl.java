0528cb7a99423ae31d4fec5715c2e031514b7621
William Serna <williamsernac@hotmail.com>
2023-01-31 14:54:39 -0500

    Update AlbumServiceImpl.java
    changed attribute name in Album entity

--- src/main/java/co/com/chartsofka/music/service/impl/AlbumServiceImpl.java
+++ src/main/java/co/com/chartsofka/music/service/impl/AlbumServiceImpl.java
@@ -1,10 +1,12 @@
 package co.com.chartsofka.music.service.impl;
 
+import co.com.chartsofka.music.controller.AlbumController;
 import co.com.chartsofka.music.dto.AlbumDTO;
 import co.com.chartsofka.music.entity.Album;
 import co.com.chartsofka.music.service.IAlbumService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,7 +16,11 @@ public class AlbumServiceImpl implements IAlbumService {
         Album r = new Album();
 
         r.setAlbumID(albumDTO.getAlbumID());
-        r.setTitle(albumDTO.getTitle());
+        r.setName(albumDTO.getName());
+        r.setTotalSongs(albumDTO.getTotalSongs());
+        r.setYearRelease(albumDTO.getYearRelease());
+        r.setArtistID(albumDTO.getArtistID());
+        r.setGenre(albumDTO.getGenre());
 
         return r;
 
@@ -22,22 +28,37 @@ public class AlbumServiceImpl implements IAlbumService {
 
     @Override
     public AlbumDTO entityToDTO(Album album) {
-        return null;
+        AlbumDTO d = new AlbumDTO();
+
+        d.setAlbumID(album.getAlbumID());
+        d.setName(album.getName());
+        d.setTotalSongs(album.getTotalSongs());
+        d.setYearRelease(album.getYearRelease());
+        d.setArtistID(album.getArtistID());
+        d.setGenre(album.getGenre());
+
+        return d;
     }
 
     @Override
     public List<AlbumDTO> getAlbums() {
+
+
+
         return null;
     }
 
     @Override
     public AlbumDTO findAlbumById(String idAlbum) {
+
+
         return null;
     }
 
     @Override
     public String saveAlbum(AlbumDTO albumDTO) {
-        return null;
+        
+        return ("Album saved");
     }
 
     @Override
