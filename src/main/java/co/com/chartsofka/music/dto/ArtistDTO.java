5c09bbd2176acf1dfc29e286c0de7d9d63ea00e4
Difsan <difsanchezru@gmail.com>
2023-01-31 13:12:26 -0500

    Artist entity, DTO, and Service was completed

--- src/main/java/co/com/chartsofka/music/dto/ArtistDTO.java
+++ src/main/java/co/com/chartsofka/music/dto/ArtistDTO.java
@@ -1,7 +1,14 @@
 package co.com.chartsofka.music.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ArtistDTO {
     private String artistID;
     private String country;
