96a11167abf9fd4c959f1bb2f8fe608ed3ff2ce6
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2023-01-31 10:54:44 -0500

    Initial commit

---
+++ src/main/java/co/com/chartsofka/music/MusicApplication.java
@@ -0,0 +1,13 @@
+package co.com.chartsofka.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/chartsofka/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.chartsofka.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
