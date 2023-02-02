deb284630c56cfda359911a87b1b2bb6c1744836
Diego Becerra <diegofer_1110@hotmail.com>
2023-02-02 13:21:44 -0500

    Relation between Album and song done

--- src/main/java/co/com/chartsofka/music/entity/Song.java
+++ src/main/java/co/com/chartsofka/music/entity/Song.java
@@ -1,19 +1,30 @@
 package co.com.chartsofka.music.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "songs")
+@Entity
 public class Song {
 
+    @GenericGenerator(name="UUID",
+            strategy = "co.com.chartsofka.music.utils.UUIDGeneratorTruncated")
+    @GeneratedValue(generator = "UUID")
+    @Id
     private String songID;
     private String name;
     private LocalTime duration;
+    @ManyToOne
+    @JsonBackReference
     private Album albumID;
     private int played;
 
