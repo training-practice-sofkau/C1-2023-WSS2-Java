e93440907cd4602c41381cd85bd30ef1f49dc4aa
BrianSammit <briansammit.cruz@gmail.com>
2023-02-02 18:39:30 -0500

    Add song entity

--- src/main/java/co/com/chartsofka/music/entity/Song.java
+++ src/main/java/co/com/chartsofka/music/entity/Song.java
@@ -1,4 +1,30 @@
 package co.com.chartsofka.music.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "songs")
 public class Song {
+    @GenericGenerator(name="UUID",
+            strategy = "co.com.chartsofka.music.utils.UUIDGeneratorTruncated")
+    @GeneratedValue(generator = "UUID")
+    @Id
+    private String songID;
+    private String name;
+    private LocalDateTime duration;
+    private Album album;
+    private Integer played;
 }
