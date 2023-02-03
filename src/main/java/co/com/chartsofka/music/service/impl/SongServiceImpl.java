8f2a99c7eb765b619ca125eedd1c3f4e91658efd
William Serna <williamsernac@hotmail.com>
2023-02-02 21:29:10 -0500

    Update SongServiceImpl.java
    Created SongServiceImpl

--- src/main/java/co/com/chartsofka/music/service/impl/SongServiceImpl.java
+++ src/main/java/co/com/chartsofka/music/service/impl/SongServiceImpl.java
@@ -1,4 +1,63 @@
 package co.com.chartsofka.music.service.impl;
 
-public class SongServiceImpl {
+import co.com.chartsofka.music.dto.SongDTO;
+import co.com.chartsofka.music.entity.Song;
+import co.com.chartsofka.music.repository.SongRepository;
+import co.com.chartsofka.music.service.ISongService;
+import co.com.chartsofka.music.utils.DTOToEntity;
+import co.com.chartsofka.music.utils.EntityToDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class SongServiceImpl implements ISongService {
+    @Autowired
+    SongRepository songRepository;
+
+    @Override
+    public Song dtoToEntity(SongDTO songDTO) {
+        return DTOToEntity.song(songDTO);
+    }
+
+    @Override
+    public SongDTO entityToDTO(Song song) {
+        return EntityToDTO.song(song);
+    }
+
+    @Override
+    public List<SongDTO> getSongs() {
+        return songRepository.findAll()
+                .stream().map(this::entityToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public SongDTO findSongById(String idSong) {
+        return entityToDTO(songRepository.findById(idSong).orElse(new Song()));
+    }
+
+    @Override
+    public SongDTO saveSong(SongDTO songDTO) {
+        return entityToDTO(songRepository.save(dtoToEntity(songDTO)));
+    }
+
+    @Override
+    public SongDTO updateSong(SongDTO songDTO) {
+
+        return entityToDTO(songRepository.save(dtoToEntity(songDTO)));
+    }
+
+    @Override
+    public String deleteSong(String idSong) {
+        Optional<Song> d = songRepository.findById(idSong);
+        if(d.isPresent()){
+            songRepository.delete(d.get());
+            return "Song deleted";
+        }
+        else return null;
+    }
 }
