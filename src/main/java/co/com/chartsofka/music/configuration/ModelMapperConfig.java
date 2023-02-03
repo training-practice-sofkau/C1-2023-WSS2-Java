f34a73c96fad8c70312fcc687756c6e0c999e8f9
Juangmcz <juangui-089@hotmail.com>
2023-02-03 06:39:38 -0500

    The name of the class was chamged.

--- src/main/java/co/com/chartsofka/music/configuration/ModelMapperConfig.java
+++ src/main/java/co/com/chartsofka/music/configuration/ModelMapperConfig.java
@@ -1,11 +1,12 @@
 package co.com.chartsofka.music.configuration;
 
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MyBeanConfiguration {
+public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
