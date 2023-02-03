e24074e65eec83c0e87dadda5f67a1e051cecdfe
BrianSammit <briansammit.cruz@gmail.com>
2023-02-02 20:04:40 -0500

    Add Song service interface

--- src/main/java/co/com/chartsofka/music/service/ISongService.java
+++ src/main/java/co/com/chartsofka/music/service/ISongService.java
@@ -1,4 +1,19 @@
 package co.com.chartsofka.music.service;
 
+
+import co.com.chartsofka.music.dto.SongDTO;
+import co.com.chartsofka.music.entity.Song;
+
+import java.util.List;
+
 public interface ISongService {
+    List<Song> getAllSongs();
+
+    SongDTO findSongById(String idArtist);
+
+    Song saveSong(SongDTO artistDTO);
+
+    SongDTO updateSong(SongDTO artistDTO);
+
+    String deleteSong(String idArtist);
 }
