3f1c18e843827f2a43ff9ac08255b2e81c3b30a5
BrianSammit <briansammit.cruz@gmail.com>
2023-02-02 20:03:05 -0500

    Add song DTO

--- src/main/java/co/com/chartsofka/music/dto/SongDTO.java
+++ src/main/java/co/com/chartsofka/music/dto/SongDTO.java
@@ -1,4 +1,14 @@
 package co.com.chartsofka.music.dto;
 
+import co.com.chartsofka.music.entity.Album;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class SongDTO {
+    private String name;
+    private LocalDateTime duration;
+//    private Album album;
+    private Integer played;
 }
