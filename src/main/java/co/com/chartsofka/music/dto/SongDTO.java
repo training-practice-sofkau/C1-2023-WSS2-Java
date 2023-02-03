c6d142cbe3e5d25f68e304083cde4bb519938221
William Serna <williamsernac@hotmail.com>
2023-02-02 21:26:54 -0500

    Update SongDTO.java
    created SongDTO class

--- src/main/java/co/com/chartsofka/music/dto/SongDTO.java
+++ src/main/java/co/com/chartsofka/music/dto/SongDTO.java
@@ -1,4 +1,17 @@
 package co.com.chartsofka.music.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongDTO {
+    private String songID;
+    private String title;
+    private LocalTime duration;
+    private Integer played;
+    private AlbumDTO album;
 }
