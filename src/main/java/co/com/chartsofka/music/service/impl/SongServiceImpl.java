0da2ed844b722bed450e473a2e5cd8d0ebde043a
BrianSammit <briansammit.cruz@gmail.com>
2023-02-02 20:05:50 -0500

    Add song service to create and get all songs

--- src/main/java/co/com/chartsofka/music/service/impl/SongServiceImpl.java
+++ src/main/java/co/com/chartsofka/music/service/impl/SongServiceImpl.java
@@ -1,4 +1,47 @@
 package co.com.chartsofka.music.service.impl;
 
-public class SongServiceImpl {
+import co.com.chartsofka.music.dto.SongDTO;
+import co.com.chartsofka.music.entity.Song;
+import co.com.chartsofka.music.repository.SongRepository;
+import co.com.chartsofka.music.service.ISongService;
+import co.com.chartsofka.music.utils.DTOToEntity;
+import co.com.chartsofka.music.utils.mapper.SongDTOToSong;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SongServiceImpl implements ISongService {
+
+    @Autowired
+    SongRepository songRepository;
+    @Autowired
+    SongDTOToSong mapper;
+
+    @Override
+    public List<Song> getAllSongs() {
+        return this.songRepository.findAll();
+    }
+
+    @Override
+    public SongDTO findSongById(String idArtist) {
+        return null;
+    }
+
+    @Override
+    public Song saveSong(SongDTO songDTO) {
+        Song song = mapper.map(songDTO);
+        return this.songRepository.save(song);
+    }
+
+    @Override
+    public SongDTO updateSong(SongDTO artistDTO) {
+        return null;
+    }
+
+    @Override
+    public String deleteSong(String idArtist) {
+        return null;
+    }
 }
