11d71dd3e894c46a12c65d1f34da17a4aee8004e
William Serna <williamsernac@hotmail.com>
2023-02-02 21:30:06 -0500

    Update ISongService.java
    Added methods to Song Service interface to be Overriden

--- src/main/java/co/com/chartsofka/music/service/ISongService.java
+++ src/main/java/co/com/chartsofka/music/service/ISongService.java
@@ -1,4 +1,23 @@
 package co.com.chartsofka.music.service;
 
+import co.com.chartsofka.music.dto.SongDTO;
+import co.com.chartsofka.music.entity.Song;
+
+import java.util.List;
+
 public interface ISongService {
+
+    Song dtoToEntity(SongDTO songDTO);
+
+    SongDTO entityToDTO(Song song);
+
+    List<SongDTO> getSongs();
+
+    SongDTO findSongById(String idSong);
+
+    SongDTO saveSong(SongDTO songDTO);
+
+    SongDTO updateSong(SongDTO songDTO);
+
+    String deleteSong(String idSong);
 }
